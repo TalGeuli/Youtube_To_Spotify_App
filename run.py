8b21cf8122a3806cf208af2235b45a86db0a118d
Tal <tal.g53@gmail.com>
2021-08-14 13:50:50 +0300

    init commit

---
+++ run.py
@@ -0,0 +1,36 @@
+import os
+
+from youtube_client import YouTubeClient
+from spotify_client import SpotifyClient
+
+def run():
+    # 1. Get a list of our playlists from youtube
+    youtube_client = YouTubeClient('./creds/client_secret.json')
+    spotify_client = SpotifyClient(os.getenv('SPOTIFY_AUTH_TOKEN'))
+    playlists = youtube_client.get_playlists()
+
+    # 2. Ask witch playlist we want to get music videos from
+    for index, playlist in enumerate(playlists):
+        print(f"{index}: {playlist.title}")
+    choice = int(input("Enter your choice: "))
+    chosen_playlist = playlists(choice)
+    print(f"You selected: {chosen_playlist.title}")
+
+    # 3. For each video in the playlist, get the song information from YouTube
+    songs = youtube_client.get_videos_from_playlist(chosen_playlist.id)
+    print(f"Attempting to add {len(songs)}")
+
+    # 4. Search for the songs on spotify
+    for song in songs:
+        spotify_song_id = spotify_client.search_song(song.artist, song.track)
+        if spotify_song_id:
+            added_song = spotify_client.add_song(spotify_song_id)
+            if added_song:
+               print (f"Added {song.artist}")
+
+    # 5. If we found the song, add it to our Spotify Liked Songs
+
+if __name__ == '__main__':
+    run()
+
+
---
+++ youtube_client.py
@@ -0,0 +1,88 @@
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import youtube_dl
+
+
+class Playlist(object):
+    def __init__(self, _id, _title):
+        self.id = _id
+        self.title = _title
+
+
+class Song(object):
+    def __init__(self, _artist, _track):
+        self.artist = _artist
+        self.track = _track
+
+
+class YouTubeClient(object):
+    def __init__(self, credentials_location):
+        scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+        # Disable OAuthlib's HTTPS verification when running locally.
+        # *DO NOT* leave this option enabled in production.
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+        api_service_name = "youtube"
+        api_version = "v3"
+
+
+        # Get credentials and create an API client
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+            credentials_location, scopes)
+        credentials = flow.run_console()
+        youtube_client = googleapiclient.discovery.build(
+            api_service_name, api_version, credentials=credentials)
+
+        self.youtube_client = youtube_client
+
+    # TODO: make more clear comments after finishing the code.
+    # goal: return a list of playlists that we would like to convert to a spotify playlists
+    def get_playlists(self):
+        request = self.youtube_client.playlists().list(
+            part="id, snippet",  # what data we what from each playlist
+            maxResult=50,  # maximum of 50 playlists
+            mine=True  # make our own playlist
+        )
+        response = request.execute()
+
+        # creat a list of playlists
+        playlists = [Playlist(item['id'], item['snippet']['title']) for item in response['item']]
+
+        return playlists
+
+    # TODO: add comments
+    # goal: to extract the videos from the playlist
+    def get_videos_from_playlist(self, playlist_id):
+        songs = []
+        request = self.youtube_client.playlistItems().list(
+            playlistId=playlist_id,
+            part="id,snippet"
+        )
+        response = request.execute()
+
+        for item in response['item']:
+            video_id = item['snippet']['resourceId']['videoId']
+            artist, track = self.get_artist_and_track_from_video(video_id)
+            if artist and track:
+                songs.append(Song(artist,track))
+
+        return songs
+
+    # TODO: make more clear comments after finishing the code.
+    # goal: to extract the artist and track from the video so we could use them later to find the same video in spotify
+    def get_artist_and_track_from_video(self, video_id):
+        youtube_url = f"https://www.youtube.com/watch?v={video_id}"
+
+        video = youtube_dl.YoutubeDL({'quiet': True}).extract_info(
+            youtube_url, download=False
+        )
+
+        artist = video['artist']
+        track = video['track']
+
+        return artist, track
+
+
