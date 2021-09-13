401ced1d199f8d60ffd2066510032cec21c6df3e
Tal <tal.g53@gmail.com>
2021-09-13 16:33:33 +0300

    add token as input

--- run.py
+++ run.py
@@ -3,32 +3,45 @@ import os
 from youtube_client import YouTubeClient
 from spotify_client import SpotifyClient
 
-def run():
-    # 1. Get a list of our playlists from youtube
-    youtube_client = YouTubeClient('./creds/client_secret.json')
-    spotify_client = SpotifyClient('BQBBgH2qIv30RN3lDgvLoxtmXcff9AFtVUuc5peoIJ-HBiZJ3iS7AQj9IHVlIyetzjo4K-gi-erKh4Yksrfinsy3R6QkO0DzxTMar62J_APN2iz-9a81CXo7-XFRwrduF7wUOSL33fAAE4aA6s4EhEjGhDKw0aUBaygx5qXe1QW7')
-    playlists = youtube_client.get_playlists()
-
-    # 2. Ask witch playlist we want to get music videos from
+# Ask witch playlist we want to get music videos from
+def choose_playlist_from_youtube(youtube_client, spotify_client, playlists):
     for index, playlist in enumerate(playlists):
         print(f"{index}: {playlist.title}")
     choice = int(input("Enter your choice: "))
     chosen_playlist = playlists[choice]
     print(f"You selected: {chosen_playlist.title}")
+    return chosen_playlist
 
-    # 3. For each video in the playlist, get the song information from YouTube
+# For each video in the playlist, get the song information from YouTube
+def get_songs_information(youtube_client, chosen_playlist):
     songs = youtube_client.get_videos_from_playlist(chosen_playlist.id)
-    print(f"Attempting to add {len(songs)}")
+    return songs
 
-    # 4. Search for the songs on spotify
-    for song in songs:
+#  Search for the songs on spotify.
+#  If we found the song, add it to our Spotify Liked Songs.
+def search_and_add_songs_to_spotify(spotify_client, songs):
+     print(f"Attempting to add {len(songs)}")
+     for song in songs:
         spotify_song_id = spotify_client.search_song(song.artist, song.track)
         if spotify_song_id:
             added_song = spotify_client.add_song(spotify_song_id)
             if added_song:
                print (f"Added {song.artist}")
 
-    # 5. If we found the song, add it to our Spotify Liked Songs
+
+# 1. Get the list of playlist from youtube.
+# 2. Choose the playlist we what to get the videos from
+# 3. Get the songs information.
+# 4. Search and add the songs to spotify.
+def run():
+    spotyfy_token = input("Enter Spotify Token: ")
+    youtube_client = YouTubeClient('./creds/client_secret.json')
+    spotify_client = SpotifyClient(spotyfy_token)
+    playlists = youtube_client.get_playlists()
+    chosen_playlist = choose_playlist_from_youtube(youtube_client, spotify_client, playlists)
+    songs_info = get_songs_information(youtube_client,chosen_playlist)
+    search_and_add_songs_to_spotify (spotify_client, songs_info)
+     
 
 if __name__ == '__main__':
     run()
